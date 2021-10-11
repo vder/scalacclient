36beeef3f9ad11da8a049ed9305822f23964aa70
vder <vder@interia.pl>
2021-10-11 13:50:07 +0200

    cleianing in Arbitraries

--- src/test/scala/com/pfl/scalacclient/arbitraries.scala
+++ src/test/scala/com/pfl/scalacclient/arbitraries.scala
@@ -2,31 +2,20 @@ package com.pfl.scalacclient
 
 import org.scalacheck.Arbitrary
 import com.pfl.scalacclient.generators._
-// import eu.timepit.refined.types.string.NonEmptyString
-// import org.scalacheck.Gen
 import com.pfl.scalacclient.model._
 
 object arbitraries {
 
-  given arbNonEmptyStringGen: Arbitrary[String] = Arbitrary(
-    nonEmptyStringGen
-  )
-  given arbRepoGen: Arbitrary[Repo] = Arbitrary(repoGen)
-  given arbOrganisationGen: Arbitrary[Organisation] = Arbitrary(
-    organisationGen
-  )
-  given arbLoginGen: Arbitrary[Login] = Arbitrary(loginGen)
-  given arbContributionsGen: Arbitrary[Contributions] = Arbitrary(
-    contributionsGen
-  )
-  given arbUserGen: Arbitrary[User] = Arbitrary(userGen)
-  given arbGithubRepoMap: Arbitrary[Map[Repo, List[User]]] =
-    Arbitrary(gitHubRepoGen)
+  given Arbitrary[String] = Arbitrary(nonEmptyStringGen)
+  given Arbitrary[Repo] = Arbitrary(repoGen)
+  given Arbitrary[Organisation] = Arbitrary(organisationGen)
+  given Arbitrary[Login] = Arbitrary(loginGen)
+  given Arbitrary[Contributions] = Arbitrary(contributionsGen)
+  given Arbitrary[User] = Arbitrary(userGen)
+  given Arbitrary[Map[Repo, List[User]]] = Arbitrary(gitHubRepoGen)
   given arbGithubOrgMap: Arbitrary[Map[Organisation, List[User]]] =
     Arbitrary(gitHubOrgGen)
-  given arbGithubMap
-      : Arbitrary[Map[Organisation, Map[Repo, List[User]]]] = Arbitrary(
-    gitHubGen
-  )
+  given arbGithubMap: Arbitrary[Map[Organisation, Map[Repo, List[User]]]] =
+    Arbitrary(gitHubGen)
 
 }
